9a99fa642092997a16a1c965d879a6429b7662cd
Houda Hamdaoui <hhouda.42.stud@gmail.com>
2021-12-06 03:16:11 +0100

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,44 @@
+#include "libft.h"
+
+int check_set(char c, char const *set)
+{
+	size_t i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	*res; 
+
+	len = ft_strlen(s1);
+	i = 0;
+	while (check_set(s1[i], set))
+		i++;
+	while (i < len && check_set(s1[len - 1], set))
+		len--;
+	res = (char*)malloc(sizeof(char) * (len - i) + 1);
+	if (!res || !s1)
+		return (NULL);
+	if (!set)
+		return (ft_memmove(res, s1, len));
+	j = 0;
+	while (i < len)
+	{
+		res[j] = s1[i];
+		i++;
+		j++;
+	}
+	res[j] = '\0';
+	return (res);
+}
--- libft.h
+++ libft.h
@@ -31,4 +31,5 @@ char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nbe, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 #endif
