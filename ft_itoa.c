bdb8dbd2f981e3a74ae70e861341f9917d284e59
Houda Hamdaoui <hhouda.42.stud@gmail.com>
2021-12-09 16:55:37 +0100

    itoa + updates

---
+++ ft_itoa.c
@@ -0,0 +1,51 @@
+#include "libft.h"
+
+int	get_size(int n)
+{
+	int count;
+	long int ln;
+
+	ln = n;
+	count = 1;
+	if (ln < 0)
+	{
+		count++;
+		ln = ln * (-1);
+	}
+	while (ln >= 10)
+	{
+		count++;
+		ln = ln / 10;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char *result;
+	int i;
+	long int  ln;
+	int size;
+
+	size = get_size(n);
+	ln = n;
+	i = 1;
+	result = (char*) malloc(sizeof(char) * size + 1);
+	if (!result)
+		return (NULL);
+	if (n < 0)
+	{
+			result[0] = '-';
+			ln = ln * (-1);
+	}
+	result[size] = '\0';
+	while (ln >= 10)
+	{
+		result[size - i] = (ln % 10) + '0';
+		ln = ln / 10;
+		i++;
+	}
+	result[size - i] = (ln % 10) + '0';
+	return (result);
+}
+
